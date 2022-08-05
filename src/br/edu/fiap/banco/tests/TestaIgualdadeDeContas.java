db19a5f5364b80b9c3ce63233cb6a4cba48c2e13
wagner <wagner.santos@accurate.com.br>
2022-08-05 15:57:17 -0300

    Adicionado ArrayLists e  testes do metodo equals e contains.

--- src/br/edu/fiap/banco/tests/TestaIgualdadeDeContas.java
+++ src/br/edu/fiap/banco/tests/TestaIgualdadeDeContas.java
@@ -1,18 +1,31 @@
 package br.edu.fiap.banco.tests;
 
+import br.edu.fiap.banco.model.Conta;
 import br.edu.fiap.banco.model.ContaCorrente;
+
+import java.util.ArrayList;
 
 public class TestaIgualdadeDeContas {
     public static void main(String[] args) {
-        ContaCorrente cc1 = new ContaCorrente(123, 456, 0.0);
-        ContaCorrente cc2 = new ContaCorrente(123, 456, 100.0);
-        ContaCorrente cc3 = new ContaCorrente(123, 457, 1000.0);
+        ArrayList<Conta> contas = new ArrayList<Conta>();
 
-        boolean ehIgual = cc1.equals(cc2);
-        System.out.println("As contas são iguais? -> " + ehIgual);
+        Conta cc1 = new ContaCorrente(123, 456, 0.0);
+        Conta cc2 = new ContaCorrente(123, 456, 100.0);
+        Conta cc3 = new ContaCorrente(123, 457, 1000.0);
 
-        boolean naoIgual = cc1.equals(cc3);
-        System.out.println("As contas são iguais? -> " + naoIgual);
+        contas.add(cc1);
+        contas.add(cc2);
+        contas.add(cc3);
 
+        for(Conta cc : contas){
+            System.out.println(cc1);
+            System.out.println(cc);
+            boolean ehIgual = cc1.equals(cc);
+            System.out.println("As contas são iguais? -> " + ehIgual);
+            System.out.println("-".repeat(60));
+        }
+
+        boolean contaEstaNaLista = contas.contains(cc1);
+        System.out.println("A conta CC1 está presente na lista de contas? -> " + contaEstaNaLista);
     }
 }
